bb670199c0550af7f7e62883a38c2bcab1038986
carlsavila <carlsavila@gmail.com>
2024-06-12 17:40:35 +0200

    Commit Fly interface, Fly methods on Eagle implemenation, compilation and test with Nature class execution

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,41 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    public void takeOff(){
+        if (!isFlying() && this.getAltitude() == 0){
+            this.flying = true;
+            System.out.printf("%s takes off into the sky.%n", this.getName());
+        }
+    }
+
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude + meters, 0);
+            System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.getAltitude());
+        }
+        return this.altitude;
+    }
+    public void glide() {
+        if (this.flying && this.altitude != 0) {
+            this.flying = true;
+            System.out.printf("%s glides into the sky.%n", this.getName());
+        }
+    }
+
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude - meters, 325);
+            System.out.printf("%s flies downward, depths : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+    public void land() {
+        if (this.flying && this.altitude <= 1) {
+            System.out.printf("%s gets down of the sky.%n", this.getName());
+        } else {
+            System.out.printf("%s is too height, it can't get down.%n", this.getName());
+        }
     }
 }
